7db000f9c764429bbebc1b4d7d3c16bb272f5815
xxzg <xyh_06@qq.com>
2026-09-04 21:41:19 +0800

    Day3 super练习

--- src/s1/w1/D3Regularpractice/Person.java
+++ src/s1/w1/D3Regularpractice/Person.java
@@ -3,5 +3,13 @@ package s1.w1.D3Regularpractice;
 public class Person {
     String name ;
     int age ;
-    public void sayHello(){System.out.println("大家好，我是"+name);}
+
+    public Person(String name,int age){
+        this.name = name ;
+        this.age = age ;
+    }
+    public void sayHello(){
+        System.out.println("大家好，我是"+name);
+    }
+
 }
--- src/s1/w1/D3Regularpractice/Student.java
+++ src/s1/w1/D3Regularpractice/Student.java
@@ -2,13 +2,17 @@ package s1.w1.D3Regularpractice;
 
 public class Student extends Person {
     int score ;
-    public void study(){System.out.println(name+"正在学习");}
+
+    public Student(String name,int age,int score){
+        super(name, age);
+        this.score = score;
+    }
+    public void study(){
+        System.out.println(name+"正在学习");
+    }
 
     public static void main(String[] args) {
-        Student s = new Student();
-        s.name = "Tom";
-        s.age = 18 ;
-        s.score = 90;
+        Student s = new Student("Tom",18,90);
         s.sayHello();
         s.study();
     }

1af2f251c90058ff7b08101eea6ded5a9b02a27e
xxzg <xyh_06@qq.com>
2026-09-04 23:02:59 +0800

    Day3 多态练习

---
+++ src/s1/w1/D3Regularpractice/AnimalDemo.java
@@ -0,0 +1,10 @@
+package s1.w1.D3Regularpractice;
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal dog = new Animal("小白");
+        System.out.println(dog);
+    }
+
+}
+
